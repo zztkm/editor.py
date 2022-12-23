57ff253318bd6308f96fbb13cefc2811c5c38d7a
zztkm <takumi.basket1682@gmail.com>
2022-12-23 19:08:35 +0900

    :tada: init

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+
+from pathlib import Path
+
+
+__version__ = "v0.1.0"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="toy editor")
+    parser.add_argument("file", type=str, help="file to open")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s {}".format(__version__))
+    args = parser.parse_args()
+
+    file = Path(args.file)
+
+    if not file.exists():
+        print(f"{file.stem} not exists")
+        sys.exit(1)
+
