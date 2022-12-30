e0b61aa0f83ef622fcffaf0307b212c9e3acfc74
zztkm <takumi.basket1682@gmail.com>
2022-12-31 07:49:35 +0900

    update

--- src/piece_table.py
+++ src/piece_table.py
@@ -17,56 +17,129 @@ class Piece:
 class PieceTable:
     def __init__(self, original_content: str):
         self.__original_content = original_content
-        self.__add_content = ""
+        self.__buffer = ""
         self.pieces = [Piece(Target.ORIGINAL, 0, len(self.__original_content))]
 
-    def get_index(self, index: int) -> int:
-        """returns the piece containing the position of index.
+    def index(self, index: int) -> str:
+        """return the character at position index.
 
         Args:
             index(int): index into the text
 
         Returs:
-            int: piece index
+            str: character at position index
+        """
+        piece_index, offset = self._get_piece_index_and_offset(index)
+        text = self._get_piece_text(piece_index)
+        return text[self.pieces[piece_index].length - (offset - index)]
+
+    def _add_buffer(self, text: str) -> int:
+        """return the position added to buffer
+
+        Args:
+            text(str): string to be added
+
+        Returs:
+            int: position added to buffer
+        """
+        added_index = len(self.__buffer)
+        self.__buffer += text
+        return added_index
+
+    def _insert_last(self, added_index: int, text_len: int):
+        last_piece = self.pieces[-1]
+        if last_piece.which == Target.ORIGINAL:
+            self.pieces.append(Piece(Target.ADD, added_index, text_len))
+        else:
+            # 最後の piece が Target.ADD だった場合
+            # 追加する文字の長さを length に足す
+            last_piece.length += text_len
+
+    def insert(self, text: str, index: int):
+        """
+        NOTE: 実装方針
+
+        INSERT 処理上必要な条件分岐は基本的にこの関数内で対応する
+
+        INSERTの仕方をユースケースと見て、ユースケースごとの関数にわける
+        """
+
+        # TODO: input の検証関数を実装する
+        if text == "":
+            return
+
+        # 書き込み用バッファに文字列を追加
+        added_index = self._add_buffer(text)
+
+        piece_index, offset = self._get_piece_index_and_offset(index)
+        if len(self.pieces) == piece_index:
+            self._insert_last(added_index, len(text))
+            return
+
+        target_piece = self.pieces[piece_index]
+
+        # text が piece の内側に位置する場合は3分割する
+        splited_pieces = [
+            Piece(target_piece.which, target_piece.start_index, offset - target_piece.start_index),
+            Piece(Target.ADD, added_index, len(text)),
+            Piece(target_piece.which, offset, target_piece.length - (offset - target_piece.start_index)),
+        ]
+        splited_pieces = list(filter(lambda p: p.length > 0, splited_pieces))
+        self.pieces = self.pieces[:piece_index] + splited_pieces + self.pieces[piece_index + 1 :]
+
+    def delete(self, index: int, length: int):
+        if length <= 0:
+            return
+
+        piece_index, offset = self._get_piece_index_and_offset(index)
+
+        if len(self.pieces) == piece_index:
+            # 存在しない位置からの削除
+            print("存在しない")
+            return
+
+        if self.pieces[piece_index].length - offset >= length:
+            # 取り回しやすいように変数化
+            piece = self.pieces[piece_index]
+            if piece.start_index == offset:
+                piece.start_index += length
+                piece.length -= length
+                return
+            elif piece.length == offset + length:
+                piece.length -= length
+                return
+
+    def get_text(self):
+        text = ""
+        for i in range(len(self.pieces)):
+            text += self._get_piece_text(i)
+        return text
+
+    def _get_piece_text(self, index: int):
+        piece = self.pieces[index]
+        if piece.which == Target.ORIGINAL:
+            return self.__original_content[piece.start_index : piece.start_index + piece.length]
+        else:
+            return self.__buffer[piece.start_index : piece.start_index + piece.length]
+
+    def _get_piece_index_and_offset(self, index: int) -> tuple[int, int]:
+        """return the piece index and offset.
+
+        Args:
+            index(int): index into the text
+
+        Returs:
+            tuple[int, int]: [piece index, offset]
         """
         if index < 0:
             raise IndexError("Not allowed to be less than 0")
 
+        offset = 0
         for i in range(len(self.pieces)):
-            if index <= self.pieces[i].length:
-                return i
-            index -= self.pieces[i].length
+            offset += self.pieces[i].length
+            if index < offset:
+                return i, self.pieces[i].length - (offset - index)
+            elif index == offset:
+                return i + 1, 0
 
-        raise IndexError("...")
-
-    def insert(self, text: str, index: int):
-        if text == "":
-            return
-
-        piece_index = self.get_index(index)
-        target_piece = self.pieces[piece_index]
-
-        # 書き込み用バッファに文字列を追加
-        added_index = len(self.__add_content)
-        self.__add_content += text
-
-        # text が piece の内側に位置する場合は3分割する
-        splited_pieces = [
-            Piece(target_piece.which, target_piece.start_index, index - target_piece.start_index),
-            Piece(Target.ADD, added_index, len(text)),
-            Piece(target_piece.which, index, target_piece.length - (index - target_piece.start_index)),
-        ]
-        self.pieces = self.pieces[:piece_index] + splited_pieces + self.pieces[piece_index + 1 :]
-
-    def delete(self, index: int, length: int):
-        print(index)
-        print(length)
-
-    def get_text(self):
-        text = ""
-        for piece in self.pieces:
-            if piece.which == Target.ORIGINAL:
-                text += self.__original_content[piece.start_index : piece.start_index + piece.length]
-            else:
-                text += self.__add_content[piece.start_index : piece.start_index + piece.length]
-        return text
+        raise IndexError("Index out of range")
--- tests/test_piece_table.py
+++ tests/test_piece_table.py
@@ -1,11 +1,22 @@
+from dataclasses import dataclass
+
 import pytest
 
 from src.piece_table import PieceTable
 
+
+@dataclass
+class InsertData:
+    index: int
+    text: str
+
+
 insert_only_once_data = [
+    ("hello", " world", 5, "hello world"),
+    ("hello", "oo", 4, "hellooo"),
     ("world", "hello ", 0, "hello world"),
     ("hello world", "vim ", 6, "hello vim world"),
-    ("tkm", " is darts player", 3, "tkm is darts player"),
+    ("zztkm", " is darts player", 5, "zztkm is darts player"),
 ]
 
 
@@ -13,5 +24,34 @@ insert_only_once_data = [
 def test_insert_only_once(original: str, add: str, index: int, expect: str):
     table = PieceTable(original)
     table.insert(add, index)
+    result = table.get_text()
+    assert result == expect
+
+
+insert_twice_data = [
+    ("hello", [InsertData(5, " world"), InsertData(11, " vim")], "hello world vim"),
+    ("hello", [InsertData(5, " world"), InsertData(6, "vim ")], "hello vim world"),
+]
+
+
+@pytest.mark.parametrize("original, insert_list, expect", insert_twice_data)
+def test_insert_twice(original: str, insert_list: list[InsertData], expect: str):
+    table = PieceTable(original)
+    for data in insert_list:
+        table.insert(data.text, data.index)
+    result = table.get_text()
+    assert result == expect
+
+
+delete_test_data = [
+    ("hello", 0, 2, "llo"),
+    ("hello", 4, 1, "hell"),
+]
+
+
+@pytest.mark.parametrize("original, index, length, expect", delete_test_data)
+def test_delete(original: str, index: int, length: int, expect: str):
+    table = PieceTable(original)
+    table.delete(index, length)
     result = table.get_text()
     assert result == expect
